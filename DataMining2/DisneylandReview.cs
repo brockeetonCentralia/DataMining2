f99070596149515505d36f55b7c38a52f24c5a71
Broc Keeton <broc.keeton@student.centralia.edu>
2025-01-21 10:13:31 -0800

    Add project files.

---
+++ DataMining2/DisneylandReview.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMining2
+{
+    public class DisneylandReview
+    {
+        [LoadColumn(0)]
+        public int ReviewId { get; set; }
+        [LoadColumn(1)]
+        public int Rating { get; set; }
+        [LoadColumn(2)]
+        public DateOnly YearMonth { get; set; }
+        [LoadColumn(3)]
+        public string ReviewerLocation { get; set; }
+        [LoadColumn(4)]
+        public string ReviewText { get; set; }
+        [LoadColumn(5)]
+        public string Branch { get; set; }
+    }
+}
---
+++ DataMining2/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML;
+using System;
+
+namespace DataMining2
+{
+    public class Program
+    {
+        private MLContext ctx;
+        private string dataPath;
+        private IDataView trainingData;
+        private string modelPath;
+        private ITransformer trainedModel;
+
+        //prediction engine (input and output types)
+        public Program()
+        {
+            //gather any variables and set them
+            dataPath = Path.Combine(Environment.CurrentDirectory, "data\\DisneylandReviews.csv");
+            modelPath = Path.Combine(Environment.CurrentDirectory, "model\\model.zip");
+
+            //create a context (connection to the database)
+            ctx = new MLContext();
+
+            //create a query to get the data
+            trainingData = ctx.Data.LoadFromTextFile<DisneylandReview>(dataPath, hasHeader: true, separatorChar: ',');
+            //build data pipeline (transforming your data into something that works)
+
+            //train your model (make it run the data)
+
+            //consume your model (use the model to make predictions)
+
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
